53138904ee6676f1ef83e6d39ea8ebbe8f35f09d
Sielarz <34028988+Sielarz@users.noreply.github.com>
2017-12-05 20:37:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var a = prompt('podaj wartosc a'),
     b = prompt('podaj wartosc b'),
-    value =  (a * a) - (2 * a * b) + (b * b);
+    value = (a * a) - (2 * a * b) + (b * b);
 
-console.log ('wynik dzialania:' + value);
+console.log('wynik dzialania:' + value);
 
 if (value > 0) {
-console.log ('wwartosc dodatnia');
+console.log('wwartosc dodatnia');
 }
 else if (value < 0) {
-console.log ('wartosc ujemna');
+console.log('wartosc ujemna');
 }
 else {
-console.log ('wartosc jest rowna 0');
+console.log('wartosc jest rowna 0');
 }
 
 
